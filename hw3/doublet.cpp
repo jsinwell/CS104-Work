fbb351989b5797a4c6ef65c2e918c1a99c110bb7
Jon Sinwell <jsinwell@usc.edu>
2021-10-15 02:15:46 -0700

    formatting

--- hw3/doublet.cpp
+++ hw3/doublet.cpp
@@ -1,10 +1,10 @@
 #include "minHeap.h"
 #include <fstream>
-#include <sstream>
-#include <string>
+#include <locale>
 #include <map>
 #include <set>
-#include <locale>
+#include <sstream>
+#include <string>
 
 using namespace std;
 
@@ -24,11 +24,11 @@ h = our heuristic value => DATA -> SALT: h = 3
 
 } */
 
-int HeuristicValue(const string& start, const string& end) { // Calculating our hueristic value
+int HeuristicValue(const string& start, const string& end) {  // Calculating our hueristic value
     int h = 0;
     int size = start.size();
-    for(int i = 0; i < size; i++) {
-        if(start[i] != end[i]) {
+    for (int i = 0; i < size; i++) {
+        if (start[i] != end[i]) {
             h++;
         }
     }
@@ -41,123 +41,115 @@ int pValue(string start, string end, int g) {
     int f = g + h;
     int priority = f * (start.size() + 1) + h;
     return priority;
-
 }
 
 // Function to make all words in file uppercase
 void ToUpper(string& word) {
     int size = word.size();
-    for(int i = 0; i < size; i++) {
-        if(islower(word[i])) {
+    for (int i = 0; i < size; i++) {
+        if (islower(word[i])) {
             word[i] = toupper(word[i]);
         }
     }
 }
 
 void GraphSearch(set<string> words, string start, string end) {
-    MinHeap<string> heap(2); // Instantiate a binary string heap
+    MinHeap<string> heap(2);  // Instantiate a binary string heap
     // g value and priority for calculations
     int g = 0;
-    int priority = pValue(start, end, g); 
+    int priority = pValue(start, end, g);
 
-    bool exit = false; // Exit condition
-    int expansions = 0; // Number of expansions recorded
+    bool exit = false;   // Exit condition
+    int expansions = 0;  // Number of expansions recorded
 
-    map<string, int> distance; // Store our steps
-      
+    map<string, int> distance;  // Store our steps
+
     set<string>::iterator it = words.find(start);
-    string curr = *it; // Store the iterator's dereferenced string values
+    string curr = *it;  // Store the iterator's dereferenced string values
     string temp;
 
-    distance[curr] = g; 
+    distance[curr] = g;
     heap.add(curr, priority);
- 
 
- try
- {   
-    while(heap.isEmpty() == false) {
-        curr = heap.peek(); // Peek the current word and explore
-        heap.remove();
-        // H = 0 meaning we found answer
-        if(HeuristicValue(curr, end) == 0) {
-            exit = true;
-            cout << distance[curr] << endl << expansions << endl;
-            break;
-        }
-        else {
-            for(unsigned int i = 0; i < curr.size(); i++) {
-                for (char j = 0; j < 26; j++) {
-                 string temp = curr;
-                 temp[i] = (char)(j+65);
+    try {
+        while (heap.isEmpty() == false) {
+            curr = heap.peek();  // Peek the current word and explore
+            heap.remove();
+            // H = 0 meaning we found answer
+            if (HeuristicValue(curr, end) == 0) {
+                exit = true;
+                cout << distance[curr] << endl << expansions << endl;
+                break;
+            } else {
+                for (unsigned int i = 0; i < curr.size(); i++) {
+                    for (char j = 0; j < 26; j++) {
+                        string temp = curr;
+                        temp[i] = (char)(j + 65);
 
-                if((distance.find(temp) == distance.end() || distance[temp] > distance[curr] + HeuristicValue(curr, temp)) && words.find(temp) != words.end())
-                {
-                    distance[temp] = distance[curr] + 1;
-                    int temph = HeuristicValue(curr, temp);
+                        if ((distance.find(temp) == distance.end()
+                             || distance[temp] > distance[curr] + HeuristicValue(curr, temp))
+                            && words.find(temp) != words.end()) {
+                            distance[temp] = distance[curr] + 1;
+                            int temph = HeuristicValue(curr, temp);
 
-                    if(temph == 1) {
-                        priority = pValue(temp, end, g);
-                        heap.add(temp, priority);
+                            if (temph == 1) {
+                                priority = pValue(temp, end, g);
+                                heap.add(temp, priority);
+                            }
+                        }
                     }
                 }
             }
+            expansions++;
         }
-    }
-    expansions++;
-}
 
         // If we reach here it means exit condition was not met and there is no transformations possible
-        if(exit == false) {
+        if (exit == false) {
             cout << "No transformation" << endl << expansions << endl;
         }
 
+    }
+
+    catch (exception& e) {  // Catch other extraneous errors
+        cout << "No transformations" << endl << expansions << endl << e.what() << endl;
+        return;
+    }
 }
 
-
-catch(exception& e) { // Catch other extraneous errors
-    cout << "No transformations" << endl << expansions << endl << e.what() << endl;
-    return;
-}
-
-}
-
-
-int main (int argc, char *argv[]) {
-    string start = argv[1]; // Store start string as the first word, end string as second word
+int main(int argc, char* argv[]) {
+    string start = argv[1];  // Store start string as the first word, end string as second word
     string end = argv[2];
-    set<string> words; // Set used to insert words into search algorithm
+    set<string> words;  // Set used to insert words into search algorithm
     string word = "";
 
-    if(start.size() != end.size()) {
+    if (start.size() != end.size()) {
         cout << "No transformation" << endl << 0 << endl;
         return 0;
     }
 
-    ToUpper(start); // Make sure start and end are uppercase to avoid errors
+    ToUpper(start);  // Make sure start and end are uppercase to avoid errors
     ToUpper(end);
 
     ifstream file;
     file.open(argv[3]);
-    if(file.fail()) {
+    if (file.fail()) {
         cout << "Cannot open file" << endl;
         throw std::exception();
     }
 
     int numWords = 0;
     file >> numWords;
-    for(int i = 0; i < numWords; i++) {
+    for (int i = 0; i < numWords; i++) {
         file >> word;
-        if(word.size() != start.size()) // Insert all the words of correct length into our string set
+        if (word.size() != start.size())  // Insert all the words of correct length into our string set
         {
             continue;
         }
-        ToUpper(word); // Make sure all our words are uppercase
+        ToUpper(word);  // Make sure all our words are uppercase
         words.insert(word);
     }
-    
 
-    GraphSearch(words,start,end);
+    GraphSearch(words, start, end);
 
     return 0;
-
 }
--- hw3/minHeap.h
+++ hw3/minHeap.h
@@ -4,157 +4,150 @@ Parent of node is at (i-1)/d
 Children of nodes are (i*d)+1 to (i*d)+d, so (i*d)+k
 */
 
-#include <iostream>
-#include <vector>
 #include <algorithm>
 #include <exception>
+#include <iostream>
+#include <vector>
 
-template <class T>
-  class MinHeap {
-     public:
-       MinHeap (int d);
-       /* Constructor that builds a d-ary Min Heap
-          This should work for any d >= 2,
-          but doesn't have to do anything for smaller d.*/
+template<class T>
+class MinHeap {
+public:
+    MinHeap(int d);
+    /* Constructor that builds a d-ary Min Heap
+       This should work for any d >= 2,
+       but doesn't have to do anything for smaller d.*/
 
-       ~MinHeap ();
+    ~MinHeap();
 
-       void add (T item, int priority);
-         /* adds the item to the heap, with the given priority. */
+    void add(T item, int priority);
+    /* adds the item to the heap, with the given priority. */
 
-       const T & peek () const;
-         /* returns the element with smallest priority.  
-			Break ties however you wish.  
-			Throws an exception if the heap is empty. */
+    const T& peek() const;
+    /* returns the element with smallest priority.
+       Break ties however you wish.
+       Throws an exception if the heap is empty. */
 
-       void remove ();
-         /* removes the element with smallest priority.
-			Break ties however you wish.
-            Throws an exception if the heap is empty. */
+    void remove();
+    /* removes the element with smallest priority.
+       Break ties however you wish.
+       Throws an exception if the heap is empty. */
 
-       bool isEmpty ();
-         /* returns true iff there are no elements on the heap. */
+    bool isEmpty();
+    /* returns true iff there are no elements on the heap. */
 
-       // void update (int location, int priority);
+    // void update (int location, int priority);
 
-   private:
-      // whatever you need to naturally store things.
-      // You may also add helper functions here.
-      std::vector<std::pair<T, int>> nodes; // Vector to store nodes and priority
-      int _d; // Max number of children for a parent node
-      int size;
-      int Parent(int i);
-      int kthChild(int i, int k);
-      int smallestChild(int pos);
-      void bubbleUp(int pos);
-      void trickleDown(int pos);
-  };
+private:
+    // whatever you need to naturally store things.
+    // You may also add helper functions here.
+    std::vector<std::pair<T, int>> nodes;  // Vector to store nodes and priority
+    int _d;                                // Max number of children for a parent node
+    int size;
+    int Parent(int i);
+    int kthChild(int i, int k);
+    int smallestChild(int pos);
+    void bubbleUp(int pos);
+    void trickleDown(int pos);
+};
 
-template <class T>
+template<class T>
 MinHeap<T>::MinHeap(int d) {
-  _d = d;
-  size = 0;
+    _d = d;
+    size = 0;
 }
 
-template <class T>
-MinHeap<T>::~MinHeap() {
-}
+template<class T>
+MinHeap<T>::~MinHeap() {}
 
-
-template <class T>
+template<class T>
 int MinHeap<T>::Parent(int i) {
-  return ((i-1) / _d);
+    return ((i - 1) / _d);
 }
 
-template <class T>
+template<class T>
 int MinHeap<T>::kthChild(int i, int k) {
-  return ((i * _d) + k);
+    return ((i * _d) + k);
 }
 
-
-template <class T>
+template<class T>
 void MinHeap<T>::add(T item, int priority) {
-  // Make a new node, increase size, and bubble up that position
-  nodes.push_back(std::make_pair(item, priority));
-  size++;
-  bubbleUp(size - 1);
+    // Make a new node, increase size, and bubble up that position
+    nodes.push_back(std::make_pair(item, priority));
+    size++;
+    bubbleUp(size - 1);
 }
 
-template <class T>
+template<class T>
 void MinHeap<T>::bubbleUp(int pos) {
-  int parent = Parent(pos); // Find the parent node to compare
+    int parent = Parent(pos);  // Find the parent node to compare
 
-  while(parent >= 0) { // Loop until we reach the root node
-    if(nodes[pos].second < nodes[parent].second) { 
-      iter_swap(nodes.begin() + pos, nodes.begin() + parent);
-      pos = parent; // Swap values only if child is less than the parent
-      parent = (pos - 1) / _d;  // New parent is one layer above curr node
+    while (parent >= 0) {  // Loop until we reach the root node
+        if (nodes[pos].second < nodes[parent].second) {
+            iter_swap(nodes.begin() + pos, nodes.begin() + parent);
+            pos = parent;             // Swap values only if child is less than the parent
+            parent = (pos - 1) / _d;  // New parent is one layer above curr node
+        } else {
+            break;
+        }
     }
-    else {
-      break;
-    }
-  }
-  
 }
 
-template <class T>
+template<class T>
 const T& MinHeap<T>::peek() const {
-  if(size == 0) { // Throw exception if heap is empty
-    throw std::exception();
-  }
-  return nodes[0].first; // Return first element of heap
+    if (size == 0) {  // Throw exception if heap is empty
+        throw std::exception();
+    }
+    return nodes[0].first;  // Return first element of heap
 }
 
-template <class T>
+template<class T>
 void MinHeap<T>::remove() {
-  if(isEmpty()) {
-    throw std::exception(); 
-  }
-  iter_swap(nodes.begin(), nodes.begin() + (size-1)); // Swap root with node to remove
-  nodes.erase(nodes.begin() + (size-1)); // Erase the root, which is now in the last element of heap
-  size--;
-  trickleDown(0); // New root is out of place now, need to trickle down
+    if (isEmpty()) {
+        throw std::exception();
+    }
+    iter_swap(nodes.begin(), nodes.begin() + (size - 1));  // Swap root with node to remove
+    nodes.erase(nodes.begin() + (size - 1));               // Erase the root, which is now in the last element of heap
+    size--;
+    trickleDown(0);  // New root is out of place now, need to trickle down
 }
 
-template <class T>
+template<class T>
 void MinHeap<T>::trickleDown(int pos) {
-  while(kthChild(pos, 1) < size) {
-    int child = smallestChild(pos); // Need to find smallest child node
-    if(nodes[child].second < nodes[pos].second) {  // Swap nodes if child is less than parent
-      iter_swap(nodes.begin() + pos, nodes.begin() + child);
-      pos = child; // Update our position to be the new child
+    while (kthChild(pos, 1) < size) {
+        int child = smallestChild(pos);                 // Need to find smallest child node
+        if (nodes[child].second < nodes[pos].second) {  // Swap nodes if child is less than parent
+            iter_swap(nodes.begin() + pos, nodes.begin() + child);
+            pos = child;  // Update our position to be the new child
+        } else {
+            break;  // Break out of loop if our node is no longer smaller than the children
+        }
     }
-    else {
-      break; // Break out of loop if our node is no longer smaller than the children
-    }
-  }
 }
 
-template <class T>
-int MinHeap<T>::smallestChild(int pos) { // Break ties 
-  int count = 2; // Variable to count through the children
-  int firstChild = kthChild(pos, 1);
-  int nextChild = kthChild(pos, count);
+template<class T>
+int MinHeap<T>::smallestChild(int pos) {  // Break ties
+    int count = 2;                        // Variable to count through the children
+    int firstChild = kthChild(pos, 1);
+    int nextChild = kthChild(pos, count);
 
-  while((count <= _d) && (nextChild < size)) { // Loop until we reach the max # of children
-    if(nodes[nextChild].second < nodes[firstChild].second) {
-      firstChild = nextChild;
+    while ((count <= _d) && (nextChild < size)) {  // Loop until we reach the max # of children
+        if (nodes[nextChild].second < nodes[firstChild].second) {
+            firstChild = nextChild;
+        }
+        count++;
+        nextChild = kthChild(pos, count);  // Increment count, update to next child node
     }
-    count++;
-    nextChild = kthChild(pos, count); // Increment count, update to next child node
-  }
 
-  return firstChild; // We return the smallest child of parent node
+    return firstChild;  // We return the smallest child of parent node
 }
 
-template <class T>
+template<class T>
 bool MinHeap<T>::isEmpty() {
-  if(size == 0) {
-    return true;
-  }
-  return false;
+    if (size == 0) {
+        return true;
+    }
+    return false;
 }
-
 
 /* template <class T>
 void MinHeap<T>::update(int location, int priority){
@@ -170,9 +163,3 @@ void MinHeap<T>::update(int location, int priority){
   }
 
   } */
-
-  
-
-
-
-
